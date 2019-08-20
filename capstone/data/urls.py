6d38156620d931b3b5e77bec6923222c69502308
joeunyeob <naramharam@naver.com>
2019-08-20 18:55:35 +0900

    user model && all data upload

--- capstone/data/urls.py
+++ capstone/data/urls.py
@@ -1,18 +1,21 @@
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from data import views
 
 # 라우터를 생성하고 뷰셋을 등록한다.
 router = DefaultRouter()
+router.register(r'all', views.AllViewSet)
 router.register(r'other', views.OtherViewSet)
 router.register(r'facilty', views.FaciltyViewSet)
 router.register(r'community', views.CommunityViewSet)
 router.register(r'professional', views.ProfessionalViewSet)
 router.register(r'jaega', views.JaeGaViewSet)
-router.register(r'users', views.UserViewSet)
+router.register(r'accounts', views.UserViewSet)
 
 # API URL을 라우터가 자동으로 인식한다.
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]

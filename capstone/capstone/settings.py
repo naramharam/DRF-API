14b26a1d681dc8d8866733a6d675a77f84a54198
joeunyeob <naramharam@naver.com>
2019-10-23 22:23:05 +0900

    도메인 생성 및 permission 추가

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -144,5 +144,8 @@ CSRF_COOKIE_SERURE = True
 # APPEND_SLASH = False
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
